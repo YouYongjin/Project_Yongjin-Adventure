7de732624a9f6794699044b095cf9eced56f5e4f
YuYongjin <radio020112@gmail.com>
2024-06-17 00:11:08 +0900

    Feat: Scene 저장, CharacterContoller Script 저장

--- YongJin_Adventure/Assets/PlayerController.cs
+++ YongJin_Adventure/Assets/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
+    public float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = 0f;
+        jumpForce = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void PlayerMove()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
     }
 }
