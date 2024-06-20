6f061948afd38c548c3557a129cc969b07479d5d
YuYongjin <radio020112@gmail.com>
2024-06-21 02:10:02 +0900

    Feat: 이동, 점프 기능 추가 및 점프 횟수 제한 기능 제작 중

--- YongJin_Adventure/Assets/PlayerController.cs
+++ YongJin_Adventure/Assets/PlayerController.cs
@@ -6,25 +6,85 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    Rigidbody2D playerRigid;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        moveSpeed = 0f;
-        jumpForce = 0f;
+        playerRigid = GetComponent<Rigidbody2D>();        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        PlayerMove();
+        MoveSpeedUp();
+        MoveStop();
+        PlayerJump();
+    }    
 
     void PlayerMove()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
+        float h = Input.GetAxisRaw("Horizontal");
+        playerRigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
+        if (playerRigid.velocity.x > moveSpeed)
+        {
+            playerRigid.velocity = new Vector2(moveSpeed, playerRigid.velocity.y);
+        }
+        else if (playerRigid.velocity.x < moveSpeed * (-1))
+        {
+            playerRigid.velocity = new Vector2(moveSpeed * (-1), playerRigid.velocity.y);
         }
     }
+
+    void MoveSpeedUp()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = moveSpeed + 5f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = moveSpeed - 5f;
+        }
+    }
+    void MoveStop()
+    {
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            playerRigid.velocity = new Vector2(playerRigid.velocity.normalized.x * 0.5f, playerRigid.velocity.y);
+        }
+    }
+
+    void PlayerJump()
+    {
+        if(jumpCheck == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                playerRigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    bool jumpCheck = false;
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.collider.gameObject.CompareTag("Ground"))
+        {
+            jumpCheck = true;
+            Debug.Log("점프 가능");
+        }
+        else
+        {
+            jumpCheck = false;
+            Debug.Log("점프 불가능");
+        }
+    }
+    //void PlayerMove()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.A))
+    //    {
+
+    //    }
+    //}
 }
