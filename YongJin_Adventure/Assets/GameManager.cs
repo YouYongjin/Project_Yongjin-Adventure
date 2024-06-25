175ba42c0f9467426f7d01154baa30aa22cac56a
YuYongjin <radio020112@gmail.com>
2024-06-26 01:28:40 +0900

    Feat: GameManager 매개변수 값 할당 삭제

--- YongJin_Adventure/Assets/GameManager.cs
+++ YongJin_Adventure/Assets/GameManager.cs
@@ -4,42 +4,18 @@ using UnityEngine;
                                                         
 public class GameManager : MonoBehaviour                
 {                                                       
-    public PlayerController playerController;           
-                                                        
-    //public Transform playerTransform;                   /* Player 캐릭터의 Transform을 참조. */
-    //public float baseHeight = 0f;                       /* 기준 높이 값을 저장. */
-                                                        
-    //public float playerYPos = 0f;                       
-    //public float resultYPos = 0f;                       
-                                                        
-    //void Awake()                                        
-    //{                                                   
-    //    playerTransform = playerController.transform;   
-    //}                                                    
-                                                        
-    //void FixedUpdate()                                  
-    //{                                                   
-    //    // Player 캐릭터의 Y축 위치.                    
-    //    playerYPos = playerTransform.position.y;        
-                                                        
-    //    //기준 높이 값을 더하여 최종 위치 계산.                                                
-    //    resultYPos = playerYPos + baseHeight;           
-                                                        
-    //    Debug.Log($"Player Y Position: {playerYPos:F2}, Base Height: {baseHeight:F2}, Result Y Position: {resultYPos:F2}");
-    //}            
-    
+    public PlayerController playerController;                  
+        
     public void PlayerCurrentLocation(Transform playerTransform, float baseHeight, float playerYPos, float resultYPos)
-    {
-        playerTransform = playerController.transform;
-        baseHeight = 0f;
-        playerYPos = playerTransform.position.y;
-        resultYPos = playerYPos + baseHeight;
-
+    {   
+        // Player 캐릭터의 Y축 위치.
         playerYPos = playerTransform.position.y;
 
-        //기준 높이 값을 더하여 최종 위치 계산.                                                
+        // 기준 높이 값을 더하여 최종 위치 계산.
         resultYPos = playerYPos + baseHeight;
 
+        // 디버그 테스트
         Debug.Log($"Player Y Position: {playerYPos:F2}, Base Height: {baseHeight:F2}, Result Y Position: {resultYPos:F2}");
-    }
-}                                                       
+    }   
+}                           
+        
